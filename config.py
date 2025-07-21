82761216f0fcaa2f66203d7075a671a935a188c5
samnguyen3115 <tnguyen6@wpi.edu>
2025-07-21 21:00:56 +0700

    refactor to a web app

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI ="postgresql://postgres:postgres@localhost:5432/lostandfound_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ROOT_PATH = basedir
+    STATIC_FOLDER = os.path.join(basedir, 'app//static')
+    TEMPLATE_FOLDER_MAIN = os.path.join(basedir, 'app//main//templates')
+    TEMPLATE_FOLDER_ERRORS = os.path.join(basedir, 'app//errors//templates')
+    TEMPLATE_FOLDER_AUTH = os.path.join(basedir, 'app//auth//templates')    
